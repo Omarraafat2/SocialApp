8db46c8f700440fd77baed40a87e215c382359d1
Omar Raafat <167703881+Omarraafat2@users.noreply.github.com>
2024-08-31 03:25:42 +0300

    Update Myposts.tsx

--- app/_Component/myPosts/Myposts.tsx
+++ app/_Component/myPosts/Myposts.tsx
@@ -13,13 +13,20 @@ import { toast } from 'react-toastify';
 import { Post, Comment } from '@/app/_interfaces/_interfaces'; // Import interfaces
 
 export default function Myposts() {
-  const { data, error, isLoading } = useUserpostsQuery();
+  // Fetch user data
+  const { data: userData, error: userError, isLoading: userLoading } = useUserDataQuery();
+  
+  // Use userData to get the dynamic user ID for fetching posts
+  const userId = userData?.user._id;
+
+  // Fetch posts with the dynamic user ID
+  const { data, error, isLoading } = useUserpostsQuery({ id: userId }, { skip: !userId });
+
   const [updatePost] = useUpdatePostMutation();
   const [deletePost] = useDeletePostMutation();
   const [addComment] = useAddCommentMutation();
   const [updateComment] = useUpdateCommentMutation();
   const [deleteComment] = useDeleteCommentMutation();
-  // const [user] = useUserDataQuery();
 
   const [editingPost, setEditingPost] = useState<string | null>(null);
   const [updatedContent, setUpdatedContent] = useState<string>('');
@@ -36,8 +43,8 @@ export default function Myposts() {
   );
   const latestPosts = sortedPosts.slice(0, 40);
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error loading posts</p>;
+  if (userLoading || isLoading) return <p>Loading...</p>;
+  if (userError || error) return <p>Error loading posts</p>;
 
   const handleUpdate = (post: Post) => {
     setEditingPost(post._id);
@@ -135,6 +142,8 @@ export default function Myposts() {
       console.error('Failed to delete comment: ', error);
     }
   };
+  console.log(latestPosts);
+  
 
   return (
     <div className="space-y-6 p-6">
@@ -198,7 +207,7 @@ export default function Myposts() {
                 )}
               </div>
             </div>
-
+    
             {/* Post Description or Edit Form */}
             {editingPost === post._id ? (
               <div>
@@ -223,97 +232,125 @@ export default function Myposts() {
             ) : (
               <p className="text-gray-800 text-base leading-relaxed mb-4">{post.body}</p>
             )}
-
+    
             {/* Post Image */}
-            {post.image && (
+            {post?.image && (
               <img
-                src={post.image}
+                src={post?.image}
                 alt="Post Image"
-                className="w-full h-[400px] rounded-lg mb-4 object-cover"
+                className="w-full h-auto rounded-md shadow-sm"
               />
             )}
-
-            {/* Comments */}
-            <div>
+    
+            {/* Comments Section */}
+            <div className="border-t border-gray-200 mt-4 pt-4">
+              {/* Toggle Comments */}
               <button
                 onClick={() => toggleComments(post._id)}
-                className="flex items-center space-x-2 text-blue-500 hover:text-blue-700 mb-2"
+                className="flex items-center text-blue-600 hover:underline"
               >
-                {visibleComments[post._id] ? <FaChevronUp /> : <FaChevronDown />}
-                <span>{visibleComments[post._id] ? 'Hide Comments' : 'Show Comments'}</span>
+                {visibleComments[post._id] ? (
+                  <>
+                    <FaChevronUp className="mr-2" /> Hide Comments
+                  </>
+                ) : (
+                  <>
+                    <FaChevronDown className="mr-2" /> Show Comments
+                  </>
+                )}
               </button>
-
-              {visibleComments[post._id] &&
-                post.comments.map((comment) => (
-                  <div key={comment._id} className="border-t border-gray-200 pt-2">
-                    {editingComment[comment._id] ? (
-                      <div className="flex flex-col mb-2">
-                        <textarea
-                          value={editContent[comment._id] || comment.content}
-                          onChange={(e) => handleEditChange(comment._id, e)}
-                          className="w-full p-2 border border-gray-300 rounded mb-2"
-                        />
-                        <button
-                          onClick={() => handleCommentEdit(comment._id, post._id)}
-                          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                        >
-                          Save
-                        </button>
-                        <button
-                          onClick={() =>
-                            setEditingComment((prev) => ({
-                              ...prev,
-                              [comment._id]: false,
-                            }))
-                          }
-                          className="bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400 ml-2"
-                        >
-                          Cancel
-                        </button>
-                      </div>
-                    ) : (
-                      <div className="flex justify-between items-center mb-2">
-                        <p className="text-gray-700">{comment.content}</p>
-                        <div className="flex items-center space-x-2">
-                          <button
-                            onClick={() =>
-                              setEditingComment((prev) => ({
-                                ...prev,
-                                [comment._id]: true,
-                              }))
-                            }
-                            className="text-blue-500 hover:text-blue-700"
-                          >
-                            <FaEdit />
-                          </button>
-                          <button
-                            onClick={() => handleCommentDelete(comment._id)}
-                            className="text-red-500 hover:text-red-700"
-                          >
-                            <FaTrash />
-                          </button>
+    
+              {/* Comments List */}
+              {visibleComments[post._id] && post.comments.length > 0 && (
+                <div className="mt-4 space-y-4">
+                  {post.comments.map((comment: Comment) => (
+                    <div key={comment._id} className="bg-gray-100 p-4 rounded-md shadow-sm">
+                      <div className="flex items-start space-x-4">
+                      <img
+                            src={userData?.user.photo || "/path/to/default-profile.jpg"}
+                            alt="Commenter"
+                            className="w-8 h-8 rounded-full object-cover border-2 border-blue-500"
+                          />
+                        <div className="flex-1">
+                          <div className="flex justify-between">
+                            <p className="text-sm font-semibold text-gray-900">{comment?.commentCreator?.name}</p>
+                            {editingComment[comment._id] ? (
+                              <div className="space-x-2">
+                                <button
+                                  onClick={() => handleCommentEdit(comment._id, post._id)}
+                                  className="text-blue-500 hover:underline"
+                                >
+                                  Save
+                                </button>
+                                <button
+                                  onClick={() =>
+                                    setEditingComment((prev) => ({
+                                      ...prev,
+                                      [comment._id]: false,
+                                    }))
+                                  }
+                                  className="text-gray-500 hover:underline"
+                                >
+                                  Cancel
+                                </button>
+                              </div>
+                            ) : (
+                              <div className="flex space-x-2">
+                                <button
+                                  onClick={() =>
+                                    setEditingComment((prev) => ({
+                                      ...prev,
+                                      [comment._id]: true,
+                                    }))
+                                  }
+                                  className="text-blue-500 hover:underline"
+                                >
+                                  <FaEdit />
+                                </button>
+                                <button
+                                  onClick={() => handleCommentDelete(comment._id)}
+                                  className="text-red-500 hover:underline"
+                                >
+                                  <FaTrash />
+                                </button>
+                              </div>
+                            )}
+                          </div>
+                          {editingComment[comment._id] ? (
+                            <textarea
+                              value={editContent[comment._id] || comment.content}
+                              onChange={(e) => handleEditChange(comment._id, e)}
+                              className="w-full mt-2 p-2 border border-gray-300 rounded"
+                            />
+                          ) : (
+                            <p className="mt-2 text-gray-800">{comment.content}</p>
+                          )}
                         </div>
                       </div>
-                    )}
-                  </div>
-                ))}
-            </div>
-
-            {/* Comment Form */}
-            <form onSubmit={(e) => handleCommentSubmit(post._id, e)}>
-              <textarea
-                value={newComment[post._id] || ''}
-                onChange={(e) => handleCommentChange(post._id, e)}
-                className="w-full p-2 border border-gray-300 rounded mb-2"
-                placeholder="Add a comment..."
-              />
-              <button
-                type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                    </div>
+                  ))}
+                </div>
+              )}
+    
+              {/* New Comment Form */}
+              <form
+                onSubmit={(e) => handleCommentSubmit(post._id, e)}
+                className="mt-6"
               >
-                Post Comment
-              </button>
-            </form>
+                <textarea
+                  value={newComment[post._id] || ''}
+                  onChange={(e) => handleCommentChange(post._id, e)}
+                  placeholder="Add a comment..."
+                  className="w-full p-2 border border-gray-300 rounded"
+                />
+                <button
+                  type="submit"
+                  className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                >
+                  Add Comment
+                </button>
+              </form>
+            </div>
           </div>
         ))
       )}
