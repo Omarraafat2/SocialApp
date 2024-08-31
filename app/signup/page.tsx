32726c5ce27a4784d56459e98b5ecf4e7dbc12a7
Omar Raafat <167703881+Omarraafat2@users.noreply.github.com>
2024-08-31 13:08:30 +0300

    Update page.tsx

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,27 +7,20 @@ import Image from "next/image";
 import { useRouter } from 'next/navigation';
 import { useSignupMutation } from '../_slice/apiSlice';
 
-
-
 export default function Signup() {
-
   const [signup, { isLoading, isError, error }] = useSignupMutation();
   const router = useRouter();
 
-
   const formik = useFormik({
     initialValues: {
-        name: '',
-        email: '',
-        password: '',
-        rePassword: '',
-        dateOfBirth: '',
-        gender: '',
+      name: '',
+      email: '',
+      password: '',
+      rePassword: '',
+      dateOfBirth: '',
+      gender: '',
     },
     validationSchema: Yup.object({
-
-
-
       name: Yup.string().required('Name is required'),
       email: Yup.string().email('Invalid email address').required('Email is required'),
       password: Yup.string()
@@ -41,131 +34,165 @@ export default function Signup() {
         .required('Confirm password is required'),
       dateOfBirth: Yup.date().required('Date of birth is required'),
       gender: Yup.string().required('Gender is required'),
-
     }),
-    onSubmit: async (value)=>{
+    onSubmit: async (value) => {
       const response = await signup(value).unwrap(); 
       if (response.message === "success") {
         router.push("/");
       } else {
         console.log(response.message);
       }
-      
     }
-})
-
-
+  });
 
   return (
     <>
-      
+      <div className="flex flex-col mb-24 md:mb-0 md:flex-row justify-center items-center h-screen">
+        {/* Image Section */}
+        <div className="order-1 md:order-2 md:w-1/2 w-full h-1/2 md:h-full flex items-center justify-center bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 text-white p-6 md:rounded-l-lg shadow-lg">
+          <div className="text-center">
+            <h1 className="text-3xl font-bold mb-4">Join the conversation!</h1>
+            <p className="text-lg">
+              Sign in to connect, share, and exchange ideas with a community that inspires.
+            </p>
+            <div className="mt-4">
+              <Image src={img} alt="description" width={300} height={300} />
+            </div>
+          </div>
+        </div>
 
-      <div  className=" flex flex-row justify-center align-middle items-center h-screen">
-
-
-    <div className='w-1/2'>
-      
-
-      <form onSubmit={formik.handleSubmit}  className="max-w-md p-8 rounded-lg bg-white mx-auto">
-            
+        {/* Form Section */}
+        <div className="order-2 md:order-1 md:w-1/2 w-full p-4 flex justify-center">
+          <form onSubmit={formik.handleSubmit} className="max-w-md p-8 rounded-lg bg-white mx-auto">
             <div className="grid md:grid-cols-2 md:gap-6">
               <div className="relative z-0 w-full mb-5 group">
-                <input value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur} type="text" name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+                <input
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  placeholder=" "
+                  required
+                />
+                <label
+                  htmlFor="name"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  First name
+                </label>
                 {formik.touched.name && formik.errors.name ? (
                   <div className="text-red-500">{formik.errors.name}</div>
                 ) : null}
               </div>
             </div>
             <div className="relative z-0 w-full mb-5 group">
-              <input value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-              <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+              <input
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                type="email"
+                name="email"
+                id="email"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                htmlFor="email"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Email address
+              </label>
               {formik.touched.email && formik.errors.email ? (
-               <div className="text-red-500">{formik.errors.email}</div>
-               ) : null}
-            
+                <div className="text-red-500">{formik.errors.email}</div>
+              ) : null}
             </div>
             <div className="relative z-0 w-full mb-5 group">
-              <input value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} type="password" name="password" id="password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-              <label htmlFor="password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
-                        
-                          {formik.touched.password && formik.errors.password ? (
-              <div className="text-red-500">{formik.errors.password}</div>
-            ) : null}
-              
-
+              <input
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                type="password"
+                name="password"
+                id="password"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                htmlFor="password"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Password
+              </label>
+              {formik.touched.password && formik.errors.password ? (
+                <div className="text-red-500">{formik.errors.password}</div>
+              ) : null}
             </div>
             <div className="relative z-0 w-full mb-5 group">
-              <input value={formik.values.rePassword} onChange={formik.handleChange} onBlur={formik.handleBlur}  type="password" name="rePassword" id="rePassword" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-              <label htmlFor="rePassword" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
-            
-              
-                            {formik.touched.rePassword && formik.errors.rePassword ? (
+              <input
+                value={formik.values.rePassword}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                type="password"
+                name="rePassword"
+                id="rePassword"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                htmlFor="rePassword"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Confirm password
+              </label>
+              {formik.touched.rePassword && formik.errors.rePassword ? (
                 <div className="text-red-500">{formik.errors.rePassword}</div>
               ) : null}
-            
             </div>
-  
             <div className="grid md:grid-cols-2 md:gap-6">
-            <div className="relative z-0 w-full mb-5 group">
-  <input
-    type="date"
-    name="dateOfBirth"
-    id="dateOfBirth"
-    value={formik.values.dateOfBirth}  
-    onChange={formik.handleChange} 
-    onBlur={formik.handleBlur} 
-    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                />
-                
-                            {formik.touched.dateOfBirth && formik.errors.dateOfBirth ? (
-              <div className="text-red-500">{formik.errors.dateOfBirth}</div>
-            ) : null}
-</div>
               <div className="relative z-0 w-full mb-5 group">
-          
-  
-            <div>
-            <select
-                    value={formik.values.gender}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    name="gender"
-                    id="gender"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  >
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                  </select>
-            </div>
-  
-  
-  
-                          </div>
+                <input
+                  type="date"
+                  name="dateOfBirth"
+                  id="dateOfBirth"
+                  value={formik.values.dateOfBirth}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+                {formik.touched.dateOfBirth && formik.errors.dateOfBirth ? (
+                  <div className="text-red-500">{formik.errors.dateOfBirth}</div>
+                ) : null}
               </div>
-              <button type="submit" className="text-black bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-      </form>
-    </div>
-
-      
-
-    <div className="w-1/2 h-full bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 text-white p-6 rounded-l-lg shadow-lg">
-        
-        <div className="pb-8">
-        <h1 className="text-3xl font-bold text-center mb-4">
-         Join the conversation!
-        </h1>
-          <p className="text-lg text-center">
-            Sign in to connect, share, and exchange ideas with a community that inspires.
-          </p>
+              <div className="relative z-0 w-full mb-5 group">
+                <select
+                  value={formik.values.gender}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  name="gender"
+                  id="gender"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                >
+                  <option value="" label="Select gender" />
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </select>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="text-black bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </form>
         </div>
-
-
-    <Image src={img} alt="description" width={700} height={700} />
       </div>
-
-      </div>
-
-   </>
-  )
+    </>
+  );
 }
